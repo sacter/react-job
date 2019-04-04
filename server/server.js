faf06b236376981b6da60f2720d73b3b9bcf63a6
me <741170471@qq.com>
2019-04-04 21:54:17 +0800

    mongodb数据库的增删改查

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-//链接mongo
-const DB_URL = 'mongodb://127.0.0.1:27017'
+//链接mongo 并且使用imooc集合
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', () => {
     console.log('mongo connect success')
 })
+// 类似于mysql的表 mongo里有文档、字段的概念
+const User = mongoose.model('user', new mongoose.Schema({
+    user: {type: String, require: true},
+    age: {type: Number, require: true}
+}))
+//新增数据
+// User.create({
+//     user: 'lee',
+//     age: 27
+// }, (err, doc) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(doc)
+//     }
+// })
+//删除数据
+// User.remove({age: 23}, (err, doc) => {
+//     console.log(doc)
+// })
+//更新数据
+// User.update({'user': 'lee'}, {'$set': {age: 123}}, (err, doc) => {
+//     if (!err) {
+//         console.log(doc)
+//     }
+// })
 
 //新建app
 const app = express()
@@ -16,7 +42,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/data', (req, res) => {
-    res.json({name: 'scater', job: 'IT321'})
+    //查找数据
+    // User.find({'user': 'scater'}, (err, doc) => {
+    //     res.json(doc)
+    // })
+    User.findOne({'user': 'scater'}, (err, doc) => {
+        res.json(doc)
+    })
+
 })
 
 app.listen(8001, () => {
