49dccb4e896ad4a8110e3c311a7074c18205014f
me <741170471@qq.com>
2019-04-16 22:59:31 +0800

    用户是否登录路由跳转

--- src/components/authroute/authroute.js
+++ src/components/authroute/authroute.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import axios from 'axios'
+import { withRouter } from 'react-router-dom'
 
+@withRouter
 class AuthRoute extends React.Component{
   componentDidMount(){
+    const publicList = ['/login', '/register']
+    const pathname = this.props.history.location.pathname
+    if (publicList.includes(pathname)) {
+      return
+    }
     // 获取用户信息
     axios.get('/user/info').then(res =>{
-      console.log(res.data)
+      if (res.status === 200) {
+        if (res.code === 0) {
+          // 有登录信息
+        } else {
+          this.props.history.push('/login')
+        }
+      }
     })
   }
   render(){
