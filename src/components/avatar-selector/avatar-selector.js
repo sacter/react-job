a025d4d26ccf259ef332b314b06c362067aca599
me <741170471@qq.com>
2019-04-22 23:14:29 +0800

    主页面架构

--- src/components/avatar-selector/avatar-selector.js
+++ src/components/avatar-selector/avatar-selector.js
@@ -1,44 +1,16 @@
 import React from 'react'
-import { Grid, List }  from 'antd-mobile'
-import PropTypes from 'prop-types' 
+import { Route } from 'react-router-dom'
 
-class AvatarSelector extends React.Component{
-  static propTypes = {
-    selectAvatar: PropTypes.func.isRequired
-  }
+class Dashboard extends React.Component{
 
-  constructor(props){
-    super(props)
-    this.state={}
-  }
   render(){
-    const avatarList = 'boy,boy-1,boy-2,boy-3,boy-4,boy-5,girl,girl-1,girl-2,girl-3,girl-4,girl-5,girl-6,girl-7,man,man-1'
-                        .split(',')
-                        .map(v => ({
-                          icon: require(`../img/${v}.png`),
-                          text: v
-                        }))
-    const gridHeader = this.state.icon
-                        ?(<div>
-                          <span>已选择头像</span>
-                          <img style={{width: 20}} src={this.state.icon}/>
-                        </div>)
-                        :'请选择头像'
     return (
       <div>
-        <List renderHeader={() => gridHeader}>
-          <Grid
-            data={avatarList}
-            activeStyle={false}
-            onClick={el => {
-              this.setState(el)
-              this.props.selectAvatar(el.text)
-            }}
-          />
-        </List>
+        <h2>header</h2>
+        <h2>footer</h2>
       </div>
     )
   }
 }
 
-export default AvatarSelector
+export default Dashboard
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDom from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './components/authroute/authroute'
 import BossInfo from './container/bossinfo/bossinfo'
 import Geniusinfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './components/dashboard/dashboard'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -19,15 +20,19 @@ const store = createStore(reducers, compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
+//boss genius me msg 4个页面
 ReactDom.render(
     (<Provider store={store}>
         <BrowserRouter>
           <div>
             <AuthRoute></AuthRoute>
-            <Route path='/bossinfo' component={BossInfo}></Route>
-            <Route path='/geniusinfo' component={Geniusinfo}></Route>
-            <Route path='/login' component={Login}></Route>
-            <Route path='/register' component={Register}></Route>
+            <Switch>
+              <Route path='/bossinfo' component={BossInfo}></Route>
+              <Route path='/geniusinfo' component={Geniusinfo}></Route>
+              <Route path='/login' component={Login}></Route>
+              <Route path='/register' component={Register}></Route>
+              <Route component={Dashboard}></Route>
+            </Switch>
           </div>
         </BrowserRouter>
     </Provider>),
