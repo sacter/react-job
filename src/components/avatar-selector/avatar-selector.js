838da5dc990e19d38faf9c863d6bda5d78014593
me <741170471@qq.com>
2019-04-22 22:58:24 +0800

    prop-typs检验头像输入

--- src/components/avatar-selector/avatar-selector.js
+++ src/components/avatar-selector/avatar-selector.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Grid, List }  from 'antd-mobile' 
+import { Grid, List }  from 'antd-mobile'
+import PropTypes from 'prop-types' 
 
 class AvatarSelector extends React.Component{
+  static propTypes = {
+    selectAvatar: PropTypes.func.isRequired
+  }
+
   constructor(props){
     super(props)
     this.state={}
