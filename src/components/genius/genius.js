dc2eef23d9e7dd96f9853831728ca588a1050cac
me <741170471@qq.com>
2019-04-24 22:44:59 +0800

    改良列表

--- src/components/genius/genius.js
+++ src/components/genius/genius.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
+import UserCard from '../usercard/usercard'
 import { getUserList } from '../../redux/chatuser.redux'
 
 @connect(
@@ -12,30 +12,7 @@ class Genius extends React.Component{
     this.props.getUserList('boss')
   }
   render(){
-    return(
-      <div className='list-container'>
-        <WingBlank>
-          {this.props.userList.map(v => (
-            v.avatar?(
-              <div key={v._id}>
-                <Card>
-                  <Card.Header
-                    title={v.user}
-                    thumb={require(`../img/${v.avatar}.png`)}
-                    extra={<span>{v.title}</span>}
-                  />
-                  <Card.Body>
-                    {v.desc.split('\n').map(v => (
-                      <div key={v}>{v}</div>
-                    ))}
-                  </Card.Body>
-                </Card>
-                <WhiteSpace/>
-              </div>):null
-          ))}
-        </WingBlank>
-      </div>
-    )
+    return <UserCard userList={this.props.userList}></UserCard>
   }
 }
 
---
+++ src/components/usercard/usercard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types' 
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
+
+class UserCard extends React.Component{
+  static propTypes = {
+    userList: PropTypes.array.isRequired
+  }
+  render(){
+    return(
+      <div className='list-container'>
+        <WingBlank>
+          {this.props.userList.map(v => (
+            v.avatar?(
+              <div key={v._id}>
+                <Card>
+                  <Card.Header
+                    title={v.user}
+                    thumb={require(`../img/${v.avatar}.png`)}
+                    extra={<span>{v.title}</span>}
+                  />
+                  <Card.Body>
+                    {v.type === 'boss'?<div className='font14 f-w-6 line-hieght-24'>公司：{v.company}</div>:null}
+                    {v.desc.split('\n').map(d => (
+                      <div className='line-hieght-24 font12' key={d}>{d}</div>
+                    ))}
+                    {v.type === 'boss'?<div className='line-hieght-24 font12'>薪资：{v.money}</div>:null}
+                  </Card.Body>
+                </Card>
+                <WhiteSpace/>
+              </div>):null
+          ))}
+        </WingBlank>
+      </div>
+    )
+  }
+}
+
+export default UserCard
