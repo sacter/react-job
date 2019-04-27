0fa7e04cac75f289f7e253f4407eb3fcb9fe5042
me <741170471@qq.com>
2019-04-27 15:32:23 +0800

    建立chat数据库表

--- server/model.js
+++ server/model.js
@@ -19,7 +19,12 @@ const models = {
     'money': {'type': String}
   },
   chart: {
-
+    'chatid': {'type': String, 'require': true},
+    'from': {'type': String, 'require': true},
+    'to': {'type': String, 'require': true},
+    'read': {'type': Boolean, 'default': false},
+    'content': {'type': String, 'require': true, 'default': ''},
+    'creat_time': {'type': Number, 'default': new Date().getTime()},
   }
 }
 
